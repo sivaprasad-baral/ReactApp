deee4c750e82cdbc9c77e5503b279be04bc6347a
sivaprasad-baral <55271551+sivaprasad-baral@users.noreply.github.com>
2021-08-30 11:39:53 +0530

    Add files via upload

---
+++ React App.py
@@ -0,0 +1,28 @@
+from itertools import combinations
+from itertools import permutations
+import xlrd
+from xlutils.copy import copy
+
+loc_variable = "D://Reverie-20210804T085618Z-001//Reverie//Selenium//variable.xls"
+loc_react_test_case = "D://Reverie-20210804T085618Z-001//Reverie//Selenium//react test cases.xls"
+
+wb = xlrd.open_workbook(loc_variable)
+sheet_variable = wb.sheet_by_index(0)
+
+attributes = ["gender", "article", "season"]
+
+templates_with_two_attribute = permutations(attributes, 2)
+
+templates_with_three_attribute = permutations(attributes, 3)
+templates_with_four_attribute = permutations(attributes, 4)
+templates_with_five_attribute = permutations(attributes, 5)
+templates_with_six_attribute = permutations(attributes, 6)
+templates_with_seven_attribute = permutations(attributes, 7)
+
+# Print the obtained permutations
+count = 0
+for i in list(templates_with_two_attribute):
+	print (i)
+	count = count + 1
+
+print(count)
